b360b56617bd77deb52dbd026e09167ab5415420
Eldan Quill <128936694+Cognitae@users.noreply.github.com>
2023-05-11 23:26:48 -0400

    First working version here is version 3.0.0! Alpha

--- email_genny.py
+++ email_genny.py
@@ -1,6 +1,8 @@
+import json
 import tkinter
 import customtkinter as ctk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk #for image handling
+import pyperclip  # for clipboard operations
 
 class TemplatesPage(ctk.CTk):
     def __init__(self, master=None):
@@ -32,32 +34,96 @@ class TemplatesPage(ctk.CTk):
         self.save_button_1.grid(row=0, column=0, padx=20, pady=(20, 0), sticky='sw')
         self.save_button_2.grid(row=1, column=0, padx=20, pady=(20, 0), sticky='sw')
         self.save_button_3.grid(row=2, column=0, padx=20, pady=(20, 0), sticky='sw')
+        
+        # Load the templates from the JSON file
+        try:
+            with open("templates.json", "r") as f:
+                templates = json.load(f)
+        except FileNotFoundError:
+            templates = {}
+
+        # Load the templates into the text boxes
+        self.template_textbox_1.insert("1.0", templates.get("template_1", ""))
+        self.template_textbox_2.insert("1.0", templates.get("template_2", ""))
+        self.template_textbox_3.insert("1.0", templates.get("template_3", ""))
 
         # Buttons
         self.back_button = ctk.CTkButton(self, text="Back", command=self.go_back)
         self.back_button.grid(row=2, column=1, padx=(20,20), pady=(180,0), sticky="se")
-
+        
     def go_back(self):
         # Call the master's go_back method
         self.master.go_back()
 
     def save_template_1(self):
-        # TODO: Implement the save logic for template 1
-        pass
+        # Get the template text
+        template_text = self.template_textbox_1.get("1.0", "end-1c")
+
+        # Load the existing templates from the JSON file
+        try:
+            with open("templates.json", "r") as f:
+                templates = json.load(f)
+        except FileNotFoundError:
+            # If the file does not exist, initialize an empty dictionary
+            templates = {}
+
+        # Update the template text in the dictionary
+        templates["template_1"] = template_text
+        
+        # Update the selected template index in the dictionary
+        templates["selected_template"] = 1
+
+        # Save the updated templates back to the JSON file
+        with open("templates.json", "w") as f:
+            json.dump(templates, f)
 
     def save_template_2(self):
-        # TODO: Implement the save logic for template 2
-        pass
+        template_text = self.template_textbox_2.get("1.0", "end-1c")
+
+        try:
+            with open("templates.json", "r") as f:
+                templates = json.load(f)
+        except FileNotFoundError:
+            templates = {}
+
+        # Update the template text in the dictionary
+        templates["template_2"] = template_text
+        
+        # Update the selected template index in the dictionary
+        templates["selected_template"] = 2
+
+
+        with open("templates.json", "w") as f:
+            json.dump(templates, f)
 
     def save_template_3(self):
-        # TODO: Implement the save logic for template 3
-        pass
+        template_text = self.template_textbox_3.get("1.0", "end-1c")
+
+        try:
+            with open("templates.json", "r") as f:
+                templates = json.load(f)
+        except FileNotFoundError:
+            templates = {}
+
+        # Update the template text in the dictionary
+        templates["template_3"] = template_text
+        
+        # Update the selected template index in the dictionary
+        templates["selected_template"] = 3
 
 
+        with open("templates.json", "w") as f:
+            json.dump(templates, f)
+            
+            
+            
 class EmailGenny(ctk.CTk):
     def __init__(self):
         super().__init__()
-
+        
+        # Initialize the selected template variable
+        self.template_var = tkinter.IntVar()
+        
         # Set up the main window
         self.title("E-Mail Genny")
         self.geometry("900x610")
@@ -71,7 +137,6 @@ class EmailGenny(ctk.CTk):
         self.template_label.grid(row=0, column=0, padx=10, pady=10)
 
         # Radio buttons for template selection
-        self.template_var = tkinter.IntVar()
         self.template_radio_1 = ctk.CTkRadioButton(self.template_frame, text="Template 1", variable=self.template_var, value=1)
         self.template_radio_2 = ctk.CTkRadioButton(self.template_frame, text="Template 2", variable=self.template_var, value=2)
         self.template_radio_3 = ctk.CTkRadioButton(self.template_frame, text="Template 3", variable=self.template_var, value=3)
@@ -82,7 +147,8 @@ class EmailGenny(ctk.CTk):
         # Textbox for the generated email
         self.email_textbox = ctk.CTkTextbox(self, width=550, height=250)
         self.email_textbox.grid(row=2, column=1, padx=(5,150), pady=(30,0))
-
+        
+        
         # Fields for the variables
         self.var_frame = ctk.CTkFrame(self)
         self.var_frame.grid(row=0, column=1, padx=(20,160), pady=20)
@@ -138,18 +204,109 @@ class EmailGenny(ctk.CTk):
         self.logo = ImageTk.PhotoImage(image) 
         self.logo_label = ctk.CTkLabel(self, image=self.logo, text="")  # Create a label with the logo
         self.logo_label.grid(row=0, column=0, padx=0, pady=0)  # Place the logo label in the grid
+        
+        self.load_templates()  # Add this line at the end of the __init__ method
+        # Load the templates from the JSON file
+        try:
+            with open("templates.json", "r") as f:
+                templates = json.load(f)
+        except FileNotFoundError:
+            templates = {}
+
+        # Load the selected template index
+        self.template_var.set(templates.get("selected_template", 1))
+
+    # This function can be put at the top of your EmailGenny class
+    @staticmethod
+    def generate_module_sentence(here_text, module):
+        if module:
+            return f"{here_text} to use in our module on {module}."
+        else:
+            return f"{here_text}."
+        
+
+    def generate_email(self):
+        # Retrieve user inputs
+        company_name = self.var_entries[0].get().strip()  # Company Name
+        name = self.var_entries[1].get().strip()  # Contact Name
+        craft = self.var_entries[2].get().strip()  # Craft
+        module = self.var_entries[3].get().strip()  # Module
+        num_images = int(self.var_entries[4].get().strip())  # Number of Images
+        image_numbers = list(range(1, num_images + 1))
+
+
+        # Determine the text based on the number of images
+        if len(image_numbers) == 1:
+            image_text = "this image"
+            here_text = "Here"
+            imag_text = "image"
+            some_text = "a"
+        else:
+            image_text = "these images"
+            imag_text = "images"
+            some_text = "some"
+            here_text_list = ["Here"] * len(image_numbers)
+            if len(here_text_list) > 2:
+                here_text = ", ".join(here_text_list[:-1]) + ", and " + here_text_list[-1]
+            else:
+                here_text = " and ".join(here_text_list)
+
+        # Generate the subject line
+        subject_line = f"Permission to use {company_name} {imag_text} for NCCER {craft} Curriculum"
+        self.subject_entry.delete(0, tkinter.END)  # Clear the previous subject line
+        self.subject_entry.insert(0, subject_line)  # Insert the new subject line
+
+        # Generate module sentence based on whether module is provided
+        module_sentence = self.generate_module_sentence(here_text, module)
+
+        # Retrieve the selected template and format it with actual values
+        selected_template_index = self.template_var.get() - 1  # Subtract 1 because the radio buttons start at 1
+        selected_template_text = [self.template_1, self.template_2, self.template_3][selected_template_index]
+
+        generated_email = selected_template_text.format(
+            name=name,
+            craft=craft,
+            image_text=image_text,
+            imag_text=imag_text,
+            some_text=some_text,
+            here_text=module_sentence,  # Use the module_sentence here
+            company_name=company_name
+        )
+        self.email_textbox.delete("1.0", tkinter.END)  # Clear the previous email body
+        self.email_textbox.insert(tkinter.END, generated_email)  # Insert the new email body
 
     def toggle_dark_mode(self):
         # Set the appearance mode based on the switch's value
         ctk.set_appearance_mode(self.appearance_mode.get())
 
-    def generate_email(self):
-        # TODO: Implement the email generation logic
-        pass
 
+    # Load the templates from the JSON file
+        self.load_templates()
+
+    def load_templates(self):
+        # Try to open the JSON file and load the templates
+        try:
+            with open("templates.json", "r") as f:
+                templates = json.load(f)
+        except FileNotFoundError:
+            # If the file does not exist, use default templates
+            templates = {
+                "template_1": "Default Template 1",
+                "template_2": "Default Template 2",
+                "template_3": "Default Template 3",
+            }
+
+        # Load the templates into variables
+        self.template_1 = templates.get("template_1", "")
+        self.template_2 = templates.get("template_2", "")
+        self.template_3 = templates.get("template_3", "")
+        
     def copy_email(self):
-        # TODO: Implement the email copying logic
-        pass
+        # Get the generated email text
+        email_text = self.email_textbox.get("1.0", "end-1c")
+
+        # Copy the email to the clipboard
+        pyperclip.copy(email_text)
 
     def edit_templates(self):
         try:
@@ -169,8 +326,9 @@ class EmailGenny(ctk.CTk):
         # Restore the previous window geometry
         self.geometry(self.prev_geometry)
         self.deiconify()
-
-# Run the application
+        self.load_templates()  # Add this line at the end of the go_back method
+            
+    # Run the application
 if __name__ == "__main__":
     app = EmailGenny()
     app.mainloop()
